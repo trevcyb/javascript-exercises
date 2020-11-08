2c774c601ec4a8ed2934714ef59466e4c012b954
trevcyb <66784125+trevcyb@users.noreply.github.com>
2020-11-08 01:33:42 -0400

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
 const repeatString = function(i,words) {
-      return(i * words);
+      let sen;
+      for(let count = 0; count <= i; count++) {
+            if(i > 0) {
+                  sen += words;
+            } else {
+                  return ("ERROR!")
+      }
+            return (sen)
 }
 
 module.exports = repeatString
